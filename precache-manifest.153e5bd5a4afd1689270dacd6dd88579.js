635896fffcf954506939d00d72e1edd3aaa7523b
Elshad <elshadaghazade@gmail.com>
2022-06-19 12:01:38 +0400

    commit

---
+++ precache-manifest.153e5bd5a4afd1689270dacd6dd88579.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b476e21265b396ee665f",
+    "url": "./static/css/main.c4d6164f.chunk.css"
+  },
+  {
+    "revision": "b476e21265b396ee665f",
+    "url": "./static/js/main.b476e212.chunk.js"
+  },
+  {
+    "revision": "06de82619331a440d5d4",
+    "url": "./static/js/1.06de8261.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "46036b4d1af221282ac07dd6bb878263",
+    "url": "./index.html"
+  }
+];
